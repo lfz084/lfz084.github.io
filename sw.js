d6dfe332904d0d394c56edc559f5a06319e26fff
163084wb@163.com <163084wb@163.com>
2024-05-28 18:43:00 +0800

    删除根目录 serviceWorker

---
+++ sw.js
@@ -0,0 +1,12 @@
+/* 五子棋小工具网站移动到 renju 子目录 */
+self.addEventListener('install', function(event) {
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+	
+});
+
+self.addEventListener('fetch', function(event) {
+	
+});
